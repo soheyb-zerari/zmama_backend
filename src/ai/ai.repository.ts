b6a3315f59b36809584ab32c1b1f6bbbf5f7caaa
Soheyb Zerari <soheyb.zerari@univ-constantine2.dz>
2025-02-04 12:56:56 +0000

    integrate api for different ai model

--- src/ai/ai.repository.ts
+++ src/ai/ai.repository.ts
@@ -1,24 +1,109 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, Logger } from '@nestjs/common';
 import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
+import Groq from 'groq-sdk';
+import OpenAI from 'openai';
+import ModelClient from '@azure-rest/ai-inference';
+import { AzureKeyCredential } from '@azure/core-auth';
 
 @Injectable()
 export class AiRepository {
-    private readonly genAi: GoogleGenerativeAI;
-    private readonly model: GenerativeModel;
+  private readonly genAi: GoogleGenerativeAI;
+  private readonly geminiModel: GenerativeModel;
+  private readonly groq: Groq;
+  private readonly openai: OpenAI;
+  private readonly deepSeek: ReturnType<typeof ModelClient>;
 
-    constructor() {
-        this.genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        this.model = this.genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
-    }
+  constructor() {
+    this.genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    this.geminiModel = this.genAi.getGenerativeModel({
+      model: 'gemini-1.5-flash',
+    });
+    this.groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+    this.openai = new OpenAI({
+      baseURL: 'https://models.inference.ai.azure.com',
+      apiKey: process.env.GIHUB_API_KEY,
+    });
+    this.deepSeek = ModelClient(
+      'https://models.inference.ai.azure.com',
+      new AzureKeyCredential(process.env.GIHUB_API_KEY),
+    );
+  }
 
-    async askAi(prompt: string) {
-        try {
-            const result = await this.model.generateContent(prompt);
-            return result.response.text();
-        } catch (err) {
-            Logger.error(err);
-            return null;
-        }
+  async askO1(prompt: string) {
+    try {
+      const response = await this.openai.chat.completions.create({
+        messages: [
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+        model: 'o1',
+      });
+
+      return response.choices[0].message.content;
+    } catch (err) {
+      Logger.error(err);
+      return null;
     }
+  }
+
+  async askO3Mini(prompt: string) {
+    try {
+      const completion = await this.openai.chat.completions.create({
+        messages: [
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+        model: 'o3-mini',
+      });
+
+      return completion.choices[0].message.content || null;
+    } catch (err) {
+      Logger.error(err);
+      return null;
+    }
+  }
+
+  async askGemini(prompt: string) {
+    try {
+      const result = await this.geminiModel.generateContent(prompt);
+      return result.response.text();
+    } catch (err) {
+      Logger.error(err);
+      return null;
+    }
+  }
+
+  async askGroq(prompt: string, model: string) {
+    try {
+      const completion = await this.groq.chat.completions.create({
+        messages: [
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+        model: model,
+      });
+
+      return (await completion.choices[0]?.message?.content) || null;
+    } catch (err) {
+      Logger.error(err);
+      return null;
+    }
+  }
+
+  async askLlama(prompt: string) {
+    const model = 'llama-3.3-70b-versatile';
+    return await this.askGroq(prompt, model);
+  }
+
+  async askMixtral(prompt: string) {
+    const model = 'mixtral-8x7b-32768';
+    return await this.askGroq(prompt, model);
+  }
 }
--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -7,8 +7,20 @@ export class AiService {
   constructor(private readonly aiRepository: AiRepository) {}
 
   async askAi(prompt: string) {
-    const response = await this.aiRepository.askAi(prompt);
-    if (!response) throw new Error('No response from AI'); 
-    return response;
+    const o3Response = await this.aiRepository.askO3Mini(prompt);
+    if (o3Response) return o3Response;
+
+    const o1Response = await this.aiRepository.askO1(prompt);
+    if (o1Response) return o1Response;
+
+    const llamaResponse = await this.aiRepository.askLlama(prompt);
+    if (llamaResponse) return llamaResponse;
+
+    const mixtralResponse = await this.aiRepository.askMixtral(prompt);
+    if (mixtralResponse) return mixtralResponse;
+
+    const geminiResponse = await this.aiRepository.askGemini(prompt);
+    if (!geminiResponse) return null;
+    return geminiResponse;
   }
 }
